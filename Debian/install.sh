3173f94ca2b8d1e307ce015a32f8e0e75e81fd4d
Alvaro Karsz <alvaro.karsz@solid-run.com>
2020-09-08 12:48:15 +0300

    f

--- Debian/install.sh
+++ Debian/install.sh
@@ -2,6 +2,6 @@
 
 echo "Downloading & Flashing Image"
 
-curl -L -k http://solidrundev.com/resources/imx8/imx8mm/imx8mm-2gb_test_image_v1.3.img.gz  --progress | gunzip | dd of=$DESTINATION bs=1M conv=fsync
+curl -L -k http://solidrundev.com/resources/imx8/imx8mm/imx8mm-2gb_test_image_v1.3.img.gzi  --progress | gunzip | dd of=$DESTINATION bs=1M conv=fsync
 
 sync
